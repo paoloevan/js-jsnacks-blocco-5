7e56c98379ec0d101ab754aff58b241e7caaa730
paoloevan <paoloev1900@gmail.com>
2022-10-24 16:07:31 +0200

    add snack 5

--- snack_5/main.js
+++ snack_5/main.js
@@ -5,3 +5,19 @@ quanto il numero di elementi dell’array).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione
 compresa tra “a” e “b”
 */
+
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const aNum = 3;
+const bNum = 7;
+//la funzione ritornerà [4,5,6,7,8]
+
+selectElement(myArray, aNum, bNum)
+
+function selectElement(array, aNum, bNum) {
+    const newArray = array.filter((element, index) => {
+        if ((index >= aNum) && (index <= bNum)) {
+            return element
+        }
+    })
+    console.log(newArray);
+}
